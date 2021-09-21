bf292541267475f5c7c1640d1f740667a6a9f6b0
LayMorja <65667728+LayMorja@users.noreply.github.com>
2021-09-21 14:16:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
+const body = document.querySelector('body');
 
 iconMenu.addEventListener('click', function() {
    iconMenu.classList.toggle('active');
    menuBody.classList.toggle('active');
+   body.classList.toggle('lock');
 })
